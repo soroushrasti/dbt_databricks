b66713be1ea03378412417b4152de475f2ff0f19
Soroush Rasti <so.rasty@gmail.com>
2026-02-11 11:16:24 +0000

    add func

--- analyses/jinjer_demo.sql
+++ analyses/jinjer_demo.sql
@@ -1,2 +1,31 @@
-{{ 'hello' }}
 
+{% set var = 2.4 -%}
+{{ var }}
+
+{% if var > 2%}
+it is hot
+{% else %}
+it is cold
+{% endif%}
+
+select
+{% set var3 = [1,4, 'a'] %}
+{%- for name in var3 -%}
+{{ name }}
+{%- if not loop.last -%}
+,
+{%- endif -%}
+
+{%- endfor %}
+from table 
+
+
+{% set v = modules.datetime.datetime.now() %}
+{{ v }}
+
+
+{% set cols = adapter.get_columns_in_relation(ref('bronze_orders')) %}
+
+{% for col in cols%}
+{{col}}
+{%endfor%}
